bd1feb482a1fe6d91432ab57177e062688377c73
Azeem Mirza <azeemrsd@gmail.com>
2020-10-31 23:34:04 +0400

    Sidebar and header added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 import { CategoriesComponent } from './layout/categories/categories.component';
 import { ProductListComponent } from './layout/product-list/product-list.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
     CategoriesComponent,
-    ProductListComponent
+    ProductListComponent,
+    SidebarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss','../../../../node_modules/bootstrap/dist/css/bootstrap.min.css']
+})
+export class SidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
